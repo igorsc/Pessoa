56e4eba474b4a176de24430d067ada96cc564d6f
igorsc <igorcesar_sc@hotmail.com>
2022-04-03 14:29:29 -0300

    Add files via upload

--- script/Pessoa.sql
+++ script/Pessoa.sql
@@ -18,6 +18,9 @@
 --
 -- Table structure for table `pessoa`
 --
+CREATE Database Pessoas;
+
+Use Pessoas;
 
 DROP TABLE IF EXISTS `pessoa`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
